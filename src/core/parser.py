81bdff55e0485bb33101f6d9cd928d72b5e091b6
ZKDev01 <rdromaguera2001@gmail.com>
2024-07-02 12:41:22 -0400

    add class lexer, parser

--- src/core/parser.py
+++ src/core/parser.py
@@ -1,13 +1,16 @@
 from tools.cmp.evaluation import evaluate_reverse_parse
+from tools.cmp.pycompiler import Grammar
 from tools.cmp.utils import Token
 from tools.parsers import LR1Parser
 from core.grammar import G
 
-def parse(tokens: list[Token]):
-  parse = __build_parser()
-  # parse(w=List[Token], shift-reduce=True)
-  # recibir el ast y aplicar evaluate_reverse_parse
-  # return ast
+class Parser:
+  def __init__(self, G: Grammar) -> None:
+    self.parse = LR1Parser(G=G, verbose=False)
 
-def __build_parser() -> LR1Parser:
-  return LR1Parser(G=G, verbose=False)
+  def parse(tokens: list[Token]):
+    pass
+    # parse(w=List[Token], shift-reduce=True)
+    # recibir el ast y aplicar evaluate_reverse_parse
+    # return ast
+
