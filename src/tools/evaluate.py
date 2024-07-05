7839b0e271e3e4d8a969f3be3797915f28d2fbe1
ZKDev01 <rdromaguera2001@gmail.com>
2024-07-05 10:14:14 -0400

    fix evaluate

--- src/tools/evaluate.py
+++ src/tools/evaluate.py
@@ -1,40 +1,43 @@
 from cmp.pycompiler import EOF
 
 def evaluate_parse(left_parse, tokens):
-    if not left_parse or not tokens:
-        return
+  if not left_parse or not tokens:
+    return
     
-    left_parse = iter(left_parse)
-    tokens = iter(tokens)
-    result = evaluate(next(left_parse), left_parse, tokens)
+  left_parse = iter(left_parse)
+  tokens = iter(tokens)
+  result = evaluate(next(left_parse), left_parse, tokens)
     
-    assert isinstance(next(tokens).token_type, EOF)
-    return result
+  assert isinstance(next(tokens).token_type, EOF)
+  return result
     
 
 def evaluate(production, left_parse, tokens, inherited_value=None):
-    head, body = production
-    attributes = production.attributes
-    
-    synteticed = [None] * (len(body) + 1) 
-    inherited = [None] * (len(body) + 1)
-    inherited[0] = inherited_value # value inherited by the head
-    
-    for i, symbol in enumerate(body, 1):
-        if symbol.IsTerminal:
-            assert inherited[i] is None
-            synteticed[i] = next(tokens).lex
-        else:
-            next_production = next(left_parse)
-            assert symbol == next_production.Left
-            
-            lambda_inherit = attributes[i] # func that compute inherited value to the next prod
-            if lambda_inherit is not None:
-                inherited[i] = lambda_inherit(inherited, synteticed)
-            synteticed[i] = evaluate(next_production, left_parse, tokens, inherited[i])
-    
-    lambda_synt = attributes[0] # func that compute the synteticed value of the head
-    if lambda_synt is None:
-        return None
+  head, body = production
+  attributes = production.attributes
+
+  # Insert your code here ...  
+  if body.IsEpsilon:
+    n = 1
+  else:
+    n = len(body._symbols)+1
+
+  # > synteticed = ...
+  # > inherited = ...
+  synteticed = [None for i in range(n)]
+  inherited = [None for i in range(n)]
+  # Anything to do with inherited_value?
+  inherited[0] = inherited_value    
+
+  for i, symbol in enumerate(body, 1):
+    if symbol.IsTerminal:
+      assert inherited[i] is None
+      synteticed[i] = next(tokens).lex
     else:
-        return lambda_synt(inherited, synteticed)
+      next_production = next(left_parse)
+      assert symbol == next_production.Left
+      if attributes[i]:
+        inherited[i] = attributes[i](inherited,synteticed)
+      synteticed[i] = evaluate(next_production, left_parse, tokens, inherited[i])
+  synteticed[0] = attributes[0](inherited,synteticed)    
+  return synteticed[0]
